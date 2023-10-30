528cbc32f0831e641b5e7a50ddac77cc3d516d0f
kenny019 <ngkenny019@gmail.com>
2023-10-31 07:33:53 +0800

    feat: add defaultValue for inputs

--- src/components/charfields.tsx
+++ src/components/charfields.tsx
@@ -17,7 +17,10 @@ const CharacterFields = () => {
           {/* name */}
           <div className="space-y-2">
             <Label htmlFor="character-name">Name</Label>
-            <Input id="character-name" value={card.characterData.data.name} />
+            <Input
+              id="character-name"
+              defaultValue={card.characterData.data.name}
+            />
           </div>
           {/* image */}
           <div className="space-y-2">
@@ -32,29 +35,44 @@ const CharacterFields = () => {
           {/* version */}
           <div className="space-y-2">
             <Label htmlFor="character-version">Version</Label>
-            <Input id="character-version" />
+            <Input
+              id="character-version"
+              defaultValue={card.characterData.data.character_version}
+            />
           </div>
           {/* creator */}
           <div className="space-y-2">
             <Label htmlFor="author">Creator</Label>
-            <Input id="author" />
+            <Input id="author" defaultValue={card.characterData.data.creator} />
           </div>
           {/* creator notes */}
           <div className="space-y-2">
             <Label htmlFor="author-notes">Creator Notes</Label>
-            <Textarea rows={5} id="author-notes" />
+            <Textarea
+              rows={5}
+              id="author-notes"
+              defaultValue={card.characterData.data.creator_notes}
+            />
           </div>
         </div>
         <div className="mt-2 space-y-2">
           {/* description */}
           <div className="flex h-[calc(50%-4px)] flex-col space-y-2">
             <Label htmlFor="character-description">Description</Label>
-            <Textarea id="character-description" className="h-full w-full" />
+            <Textarea
+              id="character-description"
+              className="h-full w-full"
+              defaultValue={card.characterData.data.description}
+            />
           </div>
           {/* first message */}
           <div className="flex h-[calc(50%-4px)] flex-col space-y-2">
             <Label htmlFor="character-first-msg">First Message</Label>
-            <Textarea id="character-first-msg" className="h-full w-full" />
+            <Textarea
+              id="character-first-msg"
+              className="h-full w-full"
+              defaultValue={card.characterData.data.first_mes}
+            />
           </div>
         </div>
         <div className="space-y-2">
@@ -63,6 +81,7 @@ const CharacterFields = () => {
           <Textarea
             id="character-example-msg"
             className="min-h-[300px] w-full lg:min-h-[700px] xl:min-h-[700px]"
+            defaultValue={card.characterData.data.mes_example}
           />
         </div>
       </div>
